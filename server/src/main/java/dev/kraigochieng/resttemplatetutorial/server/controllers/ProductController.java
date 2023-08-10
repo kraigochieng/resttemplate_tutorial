704d1eee5179d4e6bebccce880fd6843a8e3e443
kraigochieng <kraigochieng@gmail.com>
2023-08-10 12:41:47 +0300

    Created controllers

---
+++ server/src/main/java/dev/kraigochieng/resttemplatetutorial/server/controllers/ProductController.java
@@ -0,0 +1,43 @@
+package dev.kraigochieng.resttemplatetutorial.server.controllers;
+
+import dev.kraigochieng.resttemplatetutorial.server.dto.ProductCreationDTO;
+import dev.kraigochieng.resttemplatetutorial.server.dto.ProductDTO;
+import dev.kraigochieng.resttemplatetutorial.server.models.Product;
+import dev.kraigochieng.resttemplatetutorial.server.services.ProductService;
+import dev.kraigochieng.resttemplatetutorial.server.services.ProductServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "api/v1/products")
+@CrossOrigin
+public class ProductController {
+    @Autowired
+    ProductServiceImpl productService;
+
+    @GetMapping(path = "get", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<ProductDTO>> getProducts() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(productService.getProducts());
+    }
+
+    @GetMapping(path = "get/{productId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public  ResponseEntity<ProductDTO> getProduct(@PathVariable UUID productId) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(productService.getProduct(productId));
+    }
+    @PostMapping(path = "post", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ProductDTO> postProduct(@RequestBody ProductCreationDTO product) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(productService.postProduct(product));
+    }
+}
