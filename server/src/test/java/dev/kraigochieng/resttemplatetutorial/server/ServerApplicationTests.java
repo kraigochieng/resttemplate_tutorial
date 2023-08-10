e41f62d2cb3458c3d595832cd0df45daae648091
kraigochieng <kraigochieng@gmail.com>
2023-08-10 08:42:34 +0300

    Unzipped Spring Boot projects

---
+++ server/src/test/java/dev/kraigochieng/resttemplatetutorial/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kraigochieng.resttemplatetutorial.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
