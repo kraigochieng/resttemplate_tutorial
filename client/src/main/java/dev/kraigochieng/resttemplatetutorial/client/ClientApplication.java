6b5f9245e7bc535b7c418e0d686aeb1e5af4ce52
kraigochieng <kraigochieng@gmail.com>
2023-08-10 12:40:41 +0300

    Added RestTemplate bean

--- client/src/main/java/dev/kraigochieng/resttemplatetutorial/client/ClientApplication.java
+++ client/src/main/java/dev/kraigochieng/resttemplatetutorial/client/ClientApplication.java
@@ -2,6 +2,8 @@ package dev.kraigochieng.resttemplatetutorial.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ClientApplication {
@@ -10,4 +12,8 @@ public class ClientApplication {
 		SpringApplication.run(ClientApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 }
