7b4a6b88f417ceb77d71e9d246a871b6f8908515
kraigochieng <kraigochieng@gmail.com>
2023-08-10 16:20:31 +0300

    Removed unnecesary imports

--- client/src/main/java/dev/kraigochieng/resttemplatetutorial/client/services/ProductServiceImpl.java
+++ client/src/main/java/dev/kraigochieng/resttemplatetutorial/client/services/ProductServiceImpl.java
@@ -5,14 +5,12 @@ import dev.kraigochieng.resttemplatetutorial.client.dto.ProductDTO;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
